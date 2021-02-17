8e5d3ac4047c24b85cc6ef988c869d25cd28cffb
Anastasia <n41111215@mail.ru>
2021-02-17 14:37:26 +0300

    add HappyPathTest

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -6,113 +6,41 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.web.DataGenerator.Registration.generateDate;
+import static ru.netology.web.DataGenerator.Registration.generateUser;
 
 public class CardDeliveryTest {
-    LocalDate today = LocalDate.now();
-    LocalDate newDate = today.plusDays(3);
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     @BeforeEach
-    void Setup() {
+    void SetUp() {
         open("http://localhost:9999");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-
     }
 
     @Test
-    void shouldSendFormWithValidData() {
-        $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(formatter.format(newDate));
-        $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("+79258135366");
+    void shouldSendFormValid() {
+        User user = generateUser();
+        $("[data-test-id=city] input").setValue(user.getCity());
+        $(".calendar-input input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $(".calendar-input input").setValue(generateDate(8));
+        $("[data-test-id=name] input").setValue(user.getName());
+        $("[data-test-id=phone] input").setValue(user.getPhone());
         $("[data-test-id=agreement]").click();
         $(".button").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15000));
-        $(".notification__content")
-                .shouldHave(exactText("Встреча успешно забронирована на " + newDate.format(formatter)));
-    }
-
-    //
-    void shouldSendFormWithInvalidSurname() {
-        $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(formatter.format(newDate));
-        $("[data-test-id=name] input").setValue("Ivanov Иван");
-        $("[data-test-id=phone] input").setValue("+79258135366");
-        $("[data-test-id=agreement]").click();
+        $("[data-test-id=success-notification]")
+                .shouldHave(exactText("Успешно! Встреча успешно запланирована на " + generateDate(8)));
+        $(".calendar-input input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $(".calendar-input input").setValue(generateDate(9));
         $(".button").click();
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-    }
-
-    //
-    void shouldSendFormWithInvalidPhoneNumber() {
-        $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(formatter.format(newDate));
-        $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("+792581353666");
-        $("[data-test-id=agreement]").click();
-        $(".button").click();
-        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-    }
-
-    //
-    void shouldSendFormWithInvalidCity() {
-        $("[data-test-id=city] input").setValue("Moscow");
-        $("[data-test-id=date] input").sendKeys(formatter.format(newDate));
-        $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("+792581353666");
-        $("[data-test-id=agreement]").click();
-        $(".button").click();
-        $("[data-test-id=city] .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"));
-    }
-
-    //
-    void shouldSendFormWithInvalidDate() {
-        $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").doubleClick().sendKeys("11.02.2021");
-        $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("+792581353666");
-        $("[data-test-id=agreement]").click();
-        $(".button").click();
-        $("[data-test-id=date] .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
-    }
-
-    //
-    void shouldSendFormWithEmptyName() {
-        $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(formatter.format(newDate));
-        $("[data-test-id=name] input").setValue("");
-        $("[data-test-id=phone] input").setValue("+79258135366");
-        $("[data-test-id=agreement]").click();
-        $(".button").click();
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-    }
-
-    //
-    void shouldSendFormWithEmptyNumber() {
-        $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(formatter.format(newDate));
-        $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("");
-        $("[data-test-id=agreement]").click();
-        $(".button").click();
-        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-    }
-
-    //
-    void shouldSendFormWithoutCheckbox() {
-        $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(formatter.format(newDate));
-        $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("+79258135366");
-        $(".button").click();
-        $("[data-test-id='agreement'].input_invalid .checkbox__text")
-                .shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+        $(withText("Необходимо подтверждение")).shouldBe(Condition.visible, Duration.ofSeconds(15000));
+        $(".button__text").click();
+        $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(15000)).shouldHave(exactText("Успешно! Встреча успешно запланирована на " + generateDate(9)));
     }
 }
+
